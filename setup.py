cc3a11bcc9e409ecdd5caf7408d4ab56b8de3919
frey <frey@futurefamily.com>
2022-05-09 20:10:37 -0700

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "rt") as f:
 
 setuptools.setup(
     name="eset",
-    version="0.0.1",
+    version="0.0.2",
     author="Frey Waid",
     author_email="logophage1@gmail.com",
     description="Extended set",
